53ad8dc94effc9dacc39ab6ed1c6b3b25e867d4c
ahmmadSumon <arifahmmadsumon@gmail.com>
2024-12-13 15:01:22 +0600

    work pic link

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -22,9 +22,9 @@ const projects = [
       { name: "Tailwind CSS" },
       { name: "JavaScript" }
     ],
-    image: '/image/21.png',
-    live: 'https://nextjs-ecom-kappa.vercel.app/', 
-    github: 'https://github.com/ahmmadSumon/nextjs-ecom/tree/main'
+    image: '/image/11.png',
+    live: 'https://ecommercenextjs-weld.vercel.app/', 
+    github: 'https://github.com/ahmmadSumon/ecommercenextjs'
   },
   {
     num: "02",
@@ -101,6 +101,20 @@ const projects = [
     live: 'https://learnwithtrends.vercel.app/', 
     github: 'https://github.com/ahmmadSumon/learnwithtrends'
   },
+  {
+    num: "07",
+    category: "frontend",
+    title: "E-commerce Website",
+    description: "A dynamic e-commerce website built with modern technologies, offering a seamless shopping experience with a responsive design and interactive features.",
+    stack: [
+      { name: "Next.js" },
+      { name: "Tailwind CSS" },
+      { name: "JavaScript" }
+    ],
+    image: '/image/21.png',
+    live: 'https://nextjs-ecom-kappa.vercel.app/', 
+    github: 'https://github.com/ahmmadSumon/nextjs-ecom/tree/main'
+  },
 ]
 
 const Work = () => {
@@ -171,12 +185,17 @@ const Work = () => {
             >
               {projects.map((project, index) => (
                 <SwiperSlide key={index} className='w-full'>
+                    <Link href={project.live} target="_blank" rel="noopener noreferrer">
                   <div className='h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
                     <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'></div>
                     <div className='relative w-full h-full'>
+                    
+                     
                       <Image src={project.image} fill className='object-cover top-0'/>
+                   
                     </div>
                   </div>
+                  </Link>
                 </SwiperSlide>
               ))}
               <WorkSliderBtns continerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"/>
