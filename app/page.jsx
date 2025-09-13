ab976985c055b2b3ed947144bfe7e91bfa8e5431
ahmmadSumon <arifahmmadsumon@gmail.com>
2025-09-13 21:14:26 +0600

    Added project files from new PC

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,11 @@ export default function Home() {
     <section className="h-full pt-32">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none">
+         
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Web Developer</span>
             <h1 className="h1">
               Hello I'm <br />{" "}
@@ -64,9 +68,6 @@ export default function Home() {
                 />
               </div>
             </div>
-          </div>
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
           </div>
         </div>
       </div>
